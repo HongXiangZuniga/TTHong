acb50df6c53d27151bd132622f697da0b317ce8f
Hong Xiang <hongxiang17@gmail.com>
2020-07-16 18:48:20 -0400

    Update con Persistencia de datos

--- Components/EngineServices/src/main/java/com/tt/EngineServices/ModelController.java
+++ Components/EngineServices/src/main/java/com/tt/EngineServices/ModelController.java
@@ -4,6 +4,8 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 import com.fasterxml.jackson.core.JsonEncoding;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
@@ -25,10 +27,9 @@ public class ModelController {
     
     @RequestMapping("/model")
 	public HttpEntity<Model> model(
-		@RequestParam(value = "state", defaultValue = "vacio") String name,@RequestBody String body) {
-		System.out.print(body);
-		repository.deleteAll();
-		repository.save(new Model(body, body, body));
+		@RequestParam(value = "state", defaultValue = "vacio") String name,@RequestBody String body) throws JSONException {
+		JSONObject js = new JSONObject(body);
+		repository.save(new Model(js.getString("state"), js.getString("id"), js.getString("model")));
 		Model model = new Model(String.format(TEMPLATE, name));
 		model.add(linkTo(methodOn(ModelController.class).model(name, body)).withSelfRel());
 
