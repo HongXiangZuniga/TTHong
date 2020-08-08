320f99f2752c67a433d4d41f2ab882710223b582
Tu Nombre <you@example.com>
2020-08-08 19:12:20 -0400

    Update

---
+++ Components/ManagerServices/ManagerServices.py
@@ -0,0 +1,23 @@
+from flask import Flask,request
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    try:
+        content = request.get_json()
+
+        #Caso donde Inicial
+        if(len(content)==0):
+            return "Te mandare a iniciar"
+        else:
+            
+
+
+    except expression as identifier:
+        return "Hello World!"
+
+
+
+if __name__ == '__main__':
+     app.run(debug=True, port=8022)
