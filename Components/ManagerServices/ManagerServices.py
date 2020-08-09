2640c4758e4dd53e494aaa91a9407b5ff3e8a417
Hong <hongxiang17@gmail.com>
2020-08-08 21:02:21 -0400

    Update Bucle

--- Components/ManagerServices/ManagerServices.py
+++ Components/ManagerServices/ManagerServices.py
@@ -5,14 +5,19 @@ import GoTo
 
 @app.route('/')
 def hello():
+    check = True
     try:
         content = request.get_json()
         #Caso donde Inicial
         if(len(content)==0):
             return (GoTo.run(content,"EngineInit"))
-        next = GoTo.run(content,"EngineServices")
-        return next
-
+        while(GoTo.run(content,"EngineServices")!="Finish" or content.get("IModel").get("validator").size !=0 or content.get("ACModel").get("validator").size !=0 or content.get("OOMModel").get("validator").size !=0):
+            next = (GoTo.run(content,"EngineServices"))
+            link = next["next"]
+            print("Go To: " + str(link))
+            content = (GoTo.run(content,link))
+            return content
+           
     except Exception as e:
         return str(e)
 
