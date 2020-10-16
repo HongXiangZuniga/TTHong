4821086aa408d2b32ebb788521bf312a1fd0b8ef
Hong <hxian@wherex.com>
2020-10-16 20:18:34 -0300

    Add ValidatorServices

---
+++ Components/IValidatorServices/src/main/java/extra/toResponse.java
@@ -0,0 +1,21 @@
+package extra;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class JsonArray2Map {
+
+    public void transform(ArrayList<ArrayList<String>> Array, Map<String, JSONArray> Map){
+        try{
+            String aux;
+            JSONArray js = new JSONArray();
+            for(int i =0;i<Array.size();i++){
+
+            }
+        }catch(Exception e){
+            Map.put("Error","ErrorUndefined");
+        }
+    }
+}
